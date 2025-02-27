838e7e2018878aa41de7858ac6dbcf23f1e8830f
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-02-27 12:44:09 -0500

    Ajoutez des fichiers projet.

---
+++ StroopApp.Tests/ExampleTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StroopApp.Tests
+{
+    [TestClass]
+    public class ExampleTests
+    {
+        [TestMethod]
+        public void TestTrueIsTrue()
+        {
+            // Vérifie qu'une valeur vraie est bien vraie
+            Assert.IsTrue(true);
+        }
+    }
+}
