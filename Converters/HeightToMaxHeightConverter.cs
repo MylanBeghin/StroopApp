47d77a96c9406595d72edc7da1f7683be71f9a22
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-03-19 12:13:53 -0400

    Scroll des participants et petites modifications visuelles d'UI

---
+++ Converters/HeightToMaxHeightConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace StroopApp.Converters
+{
+    public class HeightToMaxHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height)
+                return height * 0.4;
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
