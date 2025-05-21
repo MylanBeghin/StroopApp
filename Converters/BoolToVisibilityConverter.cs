b4cd690057b62220aaa80163c9595e972fc46a60
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-05-21 13:57:15 -0400

    Ajout de Tooltip pour les % de switch et de congruence

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace StroopApp.Converters
+{
+    /// <summary>
+    /// Convertit un booléen en Visibility (Visible si true, Collapsed si false) et inversement.
+    /// </summary>
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && b)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+                return v == Visibility.Visible;
+            return false;
+        }
+    }
+}
