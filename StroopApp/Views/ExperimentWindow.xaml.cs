82020d926cfa4bf07896e1934c386510dd0ac8e0
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-06-05 15:52:58 -0400

    Modification de la fenêtre de configuration du profil d'une expérience : bouton flottant, ScrollViewer et DockPanel !

--- StroopApp/Views/ExperimentWindow.xaml.cs
+++ StroopApp/Views/ExperimentWindow.xaml.cs
@@ -1,16 +1,17 @@
-﻿using StroopApp.Models;
+﻿using System.Windows;
+
+using StroopApp.Models;
 using StroopApp.Services.Window;
 using StroopApp.ViewModels;
-using System.Windows;
 
 namespace StroopApp.Views
 {
-    public partial class ExperimentWindow : Window
-    {
-        public ExperimentWindow(ExperimentSettings settings, IWindowManager windowManager)
-        {
-            InitializeComponent();
-            DataContext = new ExperimentWindowViewModel(settings, new NavigationService(MainFrame), windowManager);
-        }
-    }
+	public partial class ExperimentWindow : Window
+	{
+		public ExperimentWindow(ExperimentSettings settings, IWindowManager windowManager)
+		{
+			InitializeComponent();
+			DataContext = new ExperimentWindowViewModel(settings, new NavigationService(MainFrame), windowManager);
+		}
+	}
 }
