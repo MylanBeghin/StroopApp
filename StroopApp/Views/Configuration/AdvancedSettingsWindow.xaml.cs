907d7a71f83149aa65cfb674c0eab211e451052e
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-06-09 16:15:34 -0400

    Ajout du bouton des paramètres avancés, avec une première interface simple des ports Serial !

---
+++ StroopApp/Views/Configuration/AdvancedSettingsWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+using StroopApp.ViewModels.Configuration;
+
+namespace StroopApp.Views.Configuration
+{
+	/// <summary>
+	/// Logique d'interaction pour SerialSettingsWindow.xaml
+	/// </summary>
+	public partial class AdvancedSettingsWindow : Window
+	{
+		public AdvancedSettingsWindow()
+		{
+			InitializeComponent();
+			DataContext = new AdvancedSettingsWindowViewModel();
+		}
+	}
+}
