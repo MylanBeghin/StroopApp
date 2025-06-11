2ef4a05f28fa36934e83db757f819ebb5d527026
Mylan <117998750+MylanBeghin@users.noreply.github.com>
2025-06-11 11:18:28 -0400

    Modification de la taille de la fenêtre du profil d'expérience. Ajout d'une confirmation de suppression d'un profil d'expérience

--- StroopApp/Core/ViewModelBase.cs
+++ StroopApp/Core/ViewModelBase.cs
@@ -1,29 +1,30 @@
-﻿using ModernWpf.Controls;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+
+using ModernWpf.Controls;
 
 namespace StroopApp.Core
 {
-    /// <summary>
-    /// Base class for all ViewModels, implementing <see cref="INotifyPropertyChanged"/>.
-    /// Provides a method for property change notifications and a utility method to show error dialogs.
-    /// </summary>
+	/// <summary>
+	/// Base class for all ViewModels, implementing <see cref="INotifyPropertyChanged"/>.
+	/// Provides a method for property change notifications and a utility method to show error dialogs.
+	/// </summary>
 
-    public class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        protected async void ShowErrorDialog(string message)
-        {
-            var dialog = new ContentDialog
-            {
-                Title = "Erreur",
-                Content = message,
-                CloseButtonText = "OK"
-            };
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		protected async virtual void ShowErrorDialog(string message)
+		{
+			var dialog = new ContentDialog
+			{
+				Title = "Erreur",
+				Content = message,
+				CloseButtonText = "OK"
+			};
 
-            await dialog.ShowAsync();
-        }
-    }
+			await dialog.ShowAsync();
+		}
+	}
 }
--- StroopApp/ViewModels/Configuration/Profile/ProfileManagementViewModel.cs
+++ StroopApp/ViewModels/Configuration/Profile/ProfileManagementViewModel.cs
@@ -1,114 +1,124 @@
-﻿using StroopApp.Core;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using ModernWpf.Controls;
+
+using StroopApp.Core;
 using StroopApp.Models;
 using StroopApp.Services;
 using StroopApp.Views;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
 
 namespace StroopApp.ViewModels.Configuration.Profile
 {
-    public class ProfileManagementViewModel : ViewModelBase
-    {
-        private readonly IProfileService _profileService;
-        public ObservableCollection<ExperimentProfile> Profiles
-        {
-            get; set;
-        }
+	public class ProfileManagementViewModel : ViewModelBase
+	{
+		private readonly IProfileService _profileService;
+		public ObservableCollection<ExperimentProfile> Profiles
+		{
+			get; set;
+		}
 
-        private ExperimentProfile? _currentProfile;
-        public ExperimentProfile CurrentProfile
-        {
-            get => _currentProfile;
-            set
-            {
-                if (_currentProfile != value)
-                {
-                    _currentProfile = value;
-                    OnPropertyChanged();
-                    if (_currentProfile != null)
-                        _profileService.SaveLastSelectedProfile(_currentProfile);
-                }
-            }
-        }
-        public ICommand CreateProfileCommand
-        {
-            get;
-        }
-        public ICommand ModifyProfileCommand
-        {
-            get;
-        }
-        public ICommand DeleteProfileCommand
-        {
-            get;
-        }
+		private ExperimentProfile? _currentProfile;
+		public ExperimentProfile CurrentProfile
+		{
+			get => _currentProfile;
+			set
+			{
+				if (_currentProfile != value)
+				{
+					_currentProfile = value;
+					OnPropertyChanged();
+					if (_currentProfile != null)
+						_profileService.SaveLastSelectedProfile(_currentProfile);
+				}
+			}
+		}
+		public ICommand CreateProfileCommand
+		{
+			get;
+		}
+		public ICommand ModifyProfileCommand
+		{
+			get;
+		}
+		public ICommand DeleteProfileCommand
+		{
+			get;
+		}
 
-        public ProfileManagementViewModel(IProfileService profileService)
-        {
-            _profileService = profileService;
-            Profiles = _profileService.LoadProfiles();
+		public ProfileManagementViewModel(IProfileService profileService)
+		{
+			_profileService = profileService;
+			Profiles = _profileService.LoadProfiles();
 
-            var lastId = _profileService.LoadLastSelectedProfile();
-            if (lastId.HasValue)
-                CurrentProfile = Profiles.FirstOrDefault(p => p.Id == lastId.Value);
+			var lastId = _profileService.LoadLastSelectedProfile();
+			if (lastId.HasValue)
+				CurrentProfile = Profiles.FirstOrDefault(p => p.Id == lastId.Value);
 
-            CreateProfileCommand = new RelayCommand(CreateProfile);
-            ModifyProfileCommand = new RelayCommand(ModifyProfile);
-            DeleteProfileCommand = new RelayCommand(DeleteProfile);
-        }
+			CreateProfileCommand = new RelayCommand(CreateProfile);
+			ModifyProfileCommand = new RelayCommand(ModifyProfile);
+			DeleteProfileCommand = new RelayCommand(DeleteProfile);
+		}
 
-        private void CreateProfile()
-        {
-            var newProfile = new ExperimentProfile();
-            var viewModel = new ProfileEditorViewModel(newProfile, Profiles, _profileService);
-            var profileWindow = new ProfileEditorWindow(viewModel);
-            profileWindow.ShowDialog();
-            if (profileWindow.DialogResult == true)
-            {
-                _profileService.AddProfile(newProfile, Profiles);
-                CurrentProfile = newProfile;
-            }
-        }
+		private void CreateProfile()
+		{
+			var newProfile = new ExperimentProfile();
+			var viewModel = new ProfileEditorViewModel(newProfile, Profiles, _profileService);
+			var profileWindow = new ProfileEditorWindow(viewModel);
+			profileWindow.ShowDialog();
+			if (profileWindow.DialogResult == true)
+			{
+				_profileService.AddProfile(newProfile, Profiles);
+				CurrentProfile = newProfile;
+			}
+		}
 
-        private void ModifyProfile()
-        {
-            if (CurrentProfile == null)
-            {
-                ShowErrorDialog("Veuillez sélectionner un profil à modifier !");
-                return;
-            }
-            var viewModel = new ProfileEditorViewModel(CurrentProfile, Profiles, _profileService);
-            var profileWindow = new ProfileEditorWindow(viewModel);
-            profileWindow.ShowDialog();
-            if (profileWindow.DialogResult == true)
-            {
-                _profileService.UpdateProfileById(
-                    viewModel.ModifiedProfile,
-                    viewModel.ModifiedProfile.Id,
-                    Profiles);
-                OnPropertyChanged(nameof(CurrentProfile));
-            }
-        }
+		private void ModifyProfile()
+		{
+			if (CurrentProfile == null)
+			{
+				ShowErrorDialog("Veuillez sélectionner un profil à modifier !");
+				return;
+			}
+			var viewModel = new ProfileEditorViewModel(CurrentProfile, Profiles, _profileService);
+			var profileWindow = new ProfileEditorWindow(viewModel);
+			profileWindow.ShowDialog();
+			if (profileWindow.DialogResult == true)
+			{
+				_profileService.UpdateProfileById(
+					viewModel.ModifiedProfile,
+					viewModel.ModifiedProfile.Id,
+					Profiles);
+				OnPropertyChanged(nameof(CurrentProfile));
+			}
+		}
+		private async void DeleteProfile()
+		{
+			if (CurrentProfile == null)
+			{
+				ShowErrorDialog("Veuillez sélectionner un profil à supprimer !");
+				return;
+			}
+			var dlg = new ContentDialog
+			{
+				Title = "Confirmation de suppression",
+				Content = "Voulez-vous vraiment supprimer ce profil ?",
+				PrimaryButtonText = "Supprimer",
+				CloseButtonText = "Annuler"
+			};
+			if (await dlg.ShowAsync() != ContentDialogResult.Primary)
+				return;
+			int currentIndex = Profiles.IndexOf(_currentProfile);
+			_profileService.DeleteProfile(_currentProfile, Profiles);
 
-
-        private void DeleteProfile()
-        {
-            if (CurrentProfile == null)
-            {
-                ShowErrorDialog("Veuillez sélectionner un profil à supprimer !");
-                return;
-            }
-            int currentIndex = Profiles.IndexOf(_currentProfile);
-            _profileService.DeleteProfile(_currentProfile, Profiles);
-
-            if (Profiles.Count > 0)
-            {
-                CurrentProfile = Profiles[0];
-            }
-            else
-            {
-                CurrentProfile = null;
-            }
-        }
-    }
+			if (Profiles.Count > 0)
+			{
+				CurrentProfile = Profiles[0];
+			}
+			else
+			{
+				CurrentProfile = null;
+			}
+		}
+	}
 }
